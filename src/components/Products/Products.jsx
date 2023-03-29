c04f60c0df2fd464601d882cdb2163aa3b9bd6c0
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-03-30 05:24:08 +0600

    product cart design added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./Products.css"
 const Products = ({product}) => {
-    console.log(product)
+    // console.log(product)
+    const {img, name, seller, ratings, price} = product;
     return (
-        <div>
-            
+        <div className="product">
+            <img src={img} alt="Product image" />
+            <div className="product__details">
+                <h5>{name}</h5>
+                <p>Price $ {price} </p>
+                <p>Manufacturer: {seller} </p>
+                <p>Ratings: {ratings} Starts</p>
+            </div>
+            <button className="btn_addTo_cart" >Add to Cart</button>
         </div>
     );
 };
