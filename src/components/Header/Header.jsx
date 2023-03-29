571b2a3931736ed284d03a2afd140d8fc7983bbf
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-03-30 03:10:46 +0600

    nav bar completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./Header.css";
+import logo from "../../assets/images/Logo.svg";
 const Header = () => {
     return (
-        <div className="header">
-            <h1>This is a header.</h1>
-        </div>
+        <nav className="header">
+            <img src={logo} alt="" />
+            <div>
+                <a href="/order">Order</a>
+                <a href="/order-review">Order Review</a>
+                <a href="/manage-inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
     );
 };
 
